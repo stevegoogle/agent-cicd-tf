4bca42fc3bd077e758ad64df7f635503b15806ae
Steve Lockwood <111504990+stevegoogle@users.noreply.github.com>
2026-02-11 14:05:34 -0500

    Grant public access to Cloud Run service
    Added IAM member to grant public access to the Cloud Run service.

--- terraform/cloudrun_service.tf
+++ terraform/cloudrun_service.tf
@@ -68,3 +68,13 @@ resource "google_cloud_run_v2_service" "app" {
     google_project_service.services,
   ]
 }
+
+# Make the service public so we can easily test it during this lab
+resource "google_cloud_run_v2_service_iam_member" "public_access" {
+  project  = var.project_id
+  location = var.region
+  name     = google_cloud_run_v2_service.app.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
